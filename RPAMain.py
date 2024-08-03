1351949e9f782bcbc42144b38d4a5a855e8a2b68
mijung.LEE <whiteharu@naver.com>
2024-08-03 17:03:21 +0900

    하나은행 환율조회 20240803

--- RPAMain.py
+++ RPAMain.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
-
 import time
+
+sDate = str(datetime.now().date())
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
@@ -15,21 +16,20 @@ time.sleep(2)
 # 사이트 안 프레임으로 접속
 driver.switch_to.frame('bankIframe')
 # 기간환율변동 라디오박스 클릭
-radio_box = driver.find_element(By.XPATH , '//*[@id="inqFrm"]/table/tbody/tr[1]/td/span/p/label[3]').click()
+driver.find_element(By.XPATH , '//*[@id="inqFrm"]/table/tbody/tr[1]/td/span/p/label[3]').click()
 start_box = driver.find_element(By.ID , 'tmpInqStrDt_p')
 start_box.clear()
-start_box.send_keys(str(datetime.now().date()))
+start_box.send_keys(sDate)
 end_box = driver.find_element(By.ID , 'tmpInqEndDt_p')
 end_box.clear()
-end_box.send_keys(str(datetime.now().date()))
+end_box.send_keys(sDate)
 # 통화선택
-usa_box = driver.find_element(By.ID , 'curCd')
-usa_box.send_keys('USD')
+driver.find_element(By.ID , 'curCd').send_keys('USD')
 # 고시회차 최초선택
 driver.find_element(By.XPATH, '//*[@id="inqFrm"]/table/tbody/tr[6]/td/span/p/label[1]').click()
 # 조회버튼 클릭
 search_box = driver.find_element(By.XPATH, '//*[@id="HANA_CONTENTS_DIV"]/div[2]/a/span' ).click()
-
+time.sleep(3)
 # 엑셀다운버튼 클릭
 driver.find_element(By.XPATH, '//*[@id="searchContentDiv"]/div[1]/a[2]/span').click()
 time.sleep(10)
