f708e42ba09f5c855dd8f4392f22785ca6855b04
mijung.LEE <whiteharu@naver.com>
2024-08-03 20:07:55 +0900

    오늘의 환율 메일전송 완료 20240803

--- RPAMain.py
+++ RPAMain.py
@@ -6,6 +6,10 @@ import time
 
 import pandas as pd
 
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
 sDate = str(datetime.now().date()).replace('-', '')
 print(sDate)
 '''
@@ -37,7 +41,7 @@ time.sleep(3)
 driver.find_element(By.XPATH, '//*[@id="searchContentDiv"]/div[1]/a[2]/span').click()
 time.sleep(10)
 
-'''
+
 #엑셀 파일 읽어서 매매기준율 읽어오기
 fileName = "C:/Users/LEE/Downloads/환율변동조회_" + sDate + ".xls"
 print(fileName)
@@ -45,3 +49,23 @@ print(fileName)
 df = pd.read_excel(fileName, usecols=[7], header=2)
 exchange_usa = str(df.iat[1,0])
 print(exchange_usa)
+'''
+exchange_usa = "1568"
+#네이버 메일 연결
+smtp = smtplib.SMTP('smtp.naver.com', 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login('whiteharu', 'gkfn7735!spdlqj')
+
+#네이버 메일 보내기
+my_mail = 'whiteharu@naver.com' #송신메일
+your_mail = 'whiteharu@naver.com' #수신메일
+subject = '오늘의 환율' + sDate #제목
+message = "오늘의 환율은 " + exchange_usa + " 입니다."
+msg = MIMEText(message.encode('utf-8'), _charset='utf-8')
+msg['Subject'] = Header(subject.encode('utf-8'), 'utf-8')
+msg['From'] = my_mail
+msg['To'] = your_mail
+
+smtp.sendmail(my_mail, your_mail, msg.as_string())
+smtp.quit()
